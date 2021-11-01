335da758f991b9ee25aec25bd8dd909c064b7431
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-11-01 15:24:59 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import utils
+
+numbers = [10,5,4,6,2]
+
+utils.find_max(numbers)
+
---
+++ venv/utils.py
@@ -0,0 +1,7 @@
+def find_max(array):
+    max = 0
+    for i in array:
+        if i > max:
+            max = i
+    print(max)
+
